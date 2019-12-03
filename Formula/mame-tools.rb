b07c08c9e9ea65adf3fcae94553961c7c9de31cd
הרב קייסי ג'ואל <kashaiahyah85@icloud.com>
2019-12-03 12:13:52 -0800

    Create mame-tools.rb

---
+++ Formula/mame-tools.rb
@@ -0,0 +1,66 @@
+class Mame < Formula
+  desc "Multiple Arcade Machine Emulator Tools"
+  homepage "https://mamedev.org/"
+  url "https://github.com/mamedev/mame/archive/mame0215.tar.gz"
+  version "0.215"
+  sha256 "c1b5fb0b91829df5f3dbe54ff13a7ccfa3a9f8aafa51a61c9a2f3158560ed609"
+  head "https://github.com/mamedev/mame.git"
+
+  bottle do
+    cellar :any
+    sha256 "8d6482856f7723633feed5a490addfddbb4d7fc0e525ef7e2580d388c35f0ee5" => :catalina
+    sha256 "84939184507c71fc93482c0b61097684bb75c1b5467b4701811f6182ed719aa1" => :mojave
+    sha256 "95bbfc00d59b9d9519f401761bcbbf1a852ec5cbd2591aed46927492d34053d1" => :high_sierra
+  end
+
+  depends_on "asio" => :build
+  depends_on "glm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "pugixml" => :build
+  depends_on "rapidjson" => :build
+  depends_on "sphinx-doc" => :build
+  depends_on "flac"
+  depends_on "jpeg"
+  depends_on "lua"
+  # Need C++ compiler and standard library support C++14.
+  # Build failure on Sierra, see:
+  # https://github.com/Homebrew/homebrew-core/pull/39388
+  depends_on :macos => :high_sierra
+  depends_on "portaudio"
+  depends_on "portmidi"
+  depends_on "sdl2"
+  depends_on "sqlite"
+  depends_on "utf8proc"
+
+  def install
+    inreplace "scripts/src/osd/sdl.lua", "--static", ""
+
+    # Mame's build system genie can't find headers and libraries with version suffix.
+    ENV.append "CPPFLAGS", "-I#{Formula["lua"].opt_include}/lua"
+    ENV.append "CPPFLAGS", "-I#{Formula["pugixml"].opt_include}/pugixml-1.9"
+    ENV.append "LDFLAGS", "-L#{Formula["pugixml"].opt_lib}/pugixml-1.9"
+
+    system "make", "USE_LIBSDL=1",
+                   "USE_SYSTEM_LIB_EXPAT=1",
+                   "USE_SYSTEM_LIB_ZLIB=1",
+                   "USE_SYSTEM_LIB_ASIO=1",
+                   "USE_SYSTEM_LIB_FLAC=1",
+                   "USE_SYSTEM_LIB_GLM=1",
+                   "USE_SYSTEM_LIB_JPEG=1",
+                   "USE_SYSTEM_LIB_LUA=1",
+                   "USE_SYSTEM_LIB_PORTMIDI=1",
+                   "USE_SYSTEM_LIB_PORTAUDIO=1",
+                   "USE_SYSTEM_LIB_PUGIXML=1",
+                   "USE_SYSTEM_LIB_RAPIDJSON=1",
+                   "USE_SYSTEM_LIB_SQLITE3=1",
+                   "USE_SYSTEM_LIB_UTF8PROC=1",
+                   "TOOLS=1"
+    bin.install "chdman" => "chdman"
+    pkgshare.install %w[artwork bgfx hash ini keymaps plugins samples uismall.bdf]
+  end
+
+  test do
+    assert shell_output("#{bin}/mame -help").start_with? "MAME v#{version}"
+    system "#{bin}/mame", "-validate"
+  end
+end
